ec0ee3108cf39bfaf2df75341bae09d1c1cf7ee7
liu-kai-github <31062816+liu-kai-github@users.noreply.github.com>
2020-03-16 14:43:05 +0800

    添加从 jar包内复制文件出来的功能

--- freelog-cg/src/main/java/com/freelog/cg/App.java
+++ freelog-cg/src/main/java/com/freelog/cg/App.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args) throws IOException {
 
+        CopyFile.copyFile("grammar_templates/policy_grammar.st", "grammar_templates/policy_grammar.st");
+        System.out.println("Copy '.st' file done! YoYo !");
+
         CompilerGenerator cg = new CompilerGenerator("./grammar_templates", "User", "generated_grammar/resource_policy.g4", "", "JavaScript");
         cg.renderGrammar("Resource");
         cg.parseGrammar();
